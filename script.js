85efcca14e39a534f3d31aa3faecd025004f8f54
Info <141438989+Spitzbuwe@users.noreply.github.com>
2025-03-23 20:53:25 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showSection(sectionId) {
-    document.querySelectorAll("main section").forEach(section => {
-        section.classList.add("hidden");
-    });
-    document.getElementById(sectionId).classList.remove("hidden");
-}
